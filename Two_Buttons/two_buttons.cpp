3146988f1045554d9656fdb1128b9807eaf4a618
Mark Gaballa <mgaballa@csu.fullerton.edu>
2025-05-22 22:13:18 -0700

    Two Buttons

--- Two_Buttons/two_buttons.cpp
+++ Two_Buttons/two_buttons.cpp
@@ -1,3 +1,27 @@
-#include <stdio.h>
+#include <iostream>
+
+
+
+int main() {
+	int n;
+	int m;
+	std::cin >> n >> m;
+	int count = 0; 
+
+	while (m > n) {
+		if (m%2 == 0) {
+			m = m / 2;
+		}
+		else {
+			m += 1;
+		}
+		count++;
+	}
+
+	count += (n-m);
+	std::cout << count;
+
+}
+
 
 
