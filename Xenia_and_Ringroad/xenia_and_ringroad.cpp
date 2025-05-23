c4027688a5094526249f082389dff8d182c74de5
Mark Gaballa <mgaballa@csu.fullerton.edu>
2025-05-22 23:30:01 -0700

    Successful Xenia and Ringroad

--- Xenia_and_Ringroad/xenia_and_ringroad.cpp
+++ Xenia_and_Ringroad/xenia_and_ringroad.cpp
@@ -1,27 +1,29 @@
 #include <iostream>
-#include <queue>
-
+#include <vector>
 int main(){
 	int h;
 	int j;
-	std::queue<int> tasks;
+	std::vector<int> tasks;
 	std::cin >> h >> j;
 	int x;
 	for (int i = 0; i < j; i++) {
 		std::cin >> x;
-		tasks.push(x);
+		tasks.push_back(x);
 	}
 
-	int cur = 1;
-	int count = 0;
-	while (!(tasks.empty())) {
-		if (tasks.front() == cur) {
-			tasks.pop();
-			continue;
+	long long cur = 1;
+	long long count = 0;
+	for (int i = 0; i < tasks.size(); i++) {
+		//std::cout << cur << "->" << tasks[i] << " " << count << std::endl;
+		if (cur > tasks[i]) {
+			//std::cout << ((h + 1) - cur + (tasks[i]-1)) << std::endl;
+      			count += ((h + 1) - cur + (tasks[i]-1));
+			cur = tasks[i];
 		}
-		cur = (cur % h) + 1;
-		count++;
+		else {
+      			count += (tasks[i] - cur);
+			cur = tasks[i];
+		}
 	}
-
 	std::cout << count;
 }
